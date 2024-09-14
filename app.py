d5a5fc3435245c6aa8f1db4425383ff8d2d62af0
Raffli Bisma Aryasatya <154855593+Raffli-Dev@users.noreply.github.com>
2024-09-14 18:15:18 +0700

    Perbaikan Front-End

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask,redirect,url_for,render_template,request
 
 app=Flask(__name__)
-@app.route('/',methods=['GET','POST'])
+@app.route('/',methods=['GET'])
 def home():
     return render_template('index.html')
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/kelas-tersedia',methods=['GET'])
 def Tentang():
-    return render_template('index.html')
+    return render_template('kelasTersedia.html')
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/visi-misi',methods=['GET'])
 def Syarat():
-    return render_template('index.html')
+    return render_template('visiMisi.html')
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/',methods=['GET'])
 def Contact():
-    return render_template('index.html')
+    return render_template('stater-page.html')
 
 
 if __name__ == '__main__':
