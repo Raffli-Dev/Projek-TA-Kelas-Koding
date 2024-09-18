b9992e67e7df0a04c12f93fce2c88a196505cb16
Raffli Bisma Aryasatya <154855593+Raffli-Dev@users.noreply.github.com>
2024-09-18 22:23:31 +0700

    Penghapusan bentar

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,redirect,url_for,render_template,request
+import socket
 
 app=Flask(__name__)
 @app.route('/',methods=['GET'])
@@ -17,7 +18,16 @@ def Syarat():
 def Contact():
     return render_template('stater-page.html')
 
+def generate_port(mulai_port):
+    port = mulai_port
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            if s.connect_ex(('localhost', port)) != 0:  
+                return port  
+            port += 1 
 
 if __name__ == '__main__':
-    #DEBUG is SET to TRUE. CHANGE FOR PROD
-    app.run(port=5000,debug=True)
+    mulai_port = 5000  
+    port = generate_port(mulai_port)
+    app.run(port=port, debug=True)
+    print(f'Flask running di port {port}')
